086c226383d337ee0ad99039687c73ffe72093f2
SigritPitk <sigrit.pitk@tktk.ee>
2023-01-29 20:47:21 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+//rakendus palub kasutajal sisetada tema nimi
+//rakendus tervitab kasutajat  nime pidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+//= on salvesta väärtus
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //Output
